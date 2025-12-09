7c7bca9a353cc79120e7477008a38eefa168b3ca
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-12-08 16:55:40 -0800

    add setting of logged in user to creation of recipe object

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -19,4 +19,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         
         return self.serializer_class
     
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
     
