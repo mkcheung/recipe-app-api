a25d05f1ae025962758c07ea63cbd7dfee6f0f9c
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-12-11 11:54:28 -0800

    add image to recipe model and capacity to store

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 """
 Tests for Models
 """
+from unittest.mock import patch
 from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -67,3 +68,11 @@ class ModelTests(TestCase):
         user = create_user()
         ingredient = models.Ingredient.objects.create(user=user, name='Ingredient1')
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
